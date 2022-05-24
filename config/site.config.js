ae5392b806f53fca0f5b45a2d8040f40063f7eff
Min Zhang <min@minzhang.com>
2022-05-24 13:46:52 +0800

    Update site.config.js
    Change site name

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "Min's Web Space",
+  title: "可达鸭仓库",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/onedrive-vercel-index',
